e8590a2dbb617e389d393e122880dc22b17053e7
Jawwad Ahmed <mjawwadahmed2021@gmail.com>
2024-09-08 00:37:25 +0500

    add banner images

--- ecommerce_app/lib/Screens/Home/home_widgets/slider.dart
+++ ecommerce_app/lib/Screens/Home/home_widgets/slider.dart
@@ -12,29 +12,27 @@ class _SliderviewState extends State<Sliderview> {
   int _current = 0;
   final CarouselSliderController _controller = CarouselSliderController();
   final List<String> imgList = [
-    'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-    'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
-    'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
-    'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
-    'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+    'assests/bannerimg5.png',
+    'assests/bannerimg5.png',
+    'assests/bannerimg5.png'
   ];
 
   late final List<Widget> imageSliders;
+
   void getimagesliders() {
     imageSliders = imgList
         .map((item) => Container(
-              child: Container(
-                margin: EdgeInsets.all(5.0),
-                child: ClipRRect(
-                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                    child: Stack(
-                      children: <Widget>[
-                        Image.network(item, fit: BoxFit.cover, width: 1000.0),
-                        Positioned(
-                          bottom: 0.0,
-                          left: 0.0,
-                          right: 0.0,
-                          child: Container(
+              margin: const EdgeInsets.all(0),
+              child: ClipRRect(
+                  borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+                  child: Stack(
+                    children: <Widget>[
+                      Image.asset(item, fit: BoxFit.cover, width: 1000.0),
+                      Positioned(
+                        bottom: 0.0,
+                        left: 0.0,
+                        right: 0.0,
+                        child: Container(
                             decoration: const BoxDecoration(
                               gradient: LinearGradient(
                                 colors: [
@@ -47,19 +45,10 @@ class _SliderviewState extends State<Sliderview> {
                             ),
                             padding: const EdgeInsets.symmetric(
                                 vertical: 10.0, horizontal: 20.0),
-                            child: Text(
-                              'No. ${imgList.indexOf(item)} image',
-                              style: const TextStyle(
-                                color: Colors.white,
-                                fontSize: 20.0,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    )),
-              ),
+                            child: Text('')),
+                      ),
+                    ],
+                  )),
             ))
         .toList();
   }
@@ -72,23 +61,28 @@ class _SliderviewState extends State<Sliderview> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Column(children: [
-          Expanded(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Image Slider'),
+      ),
+      body: Column(
+        children: [
+          // Wrap the CarouselSlider with SizedBox to control height
+          Container(
+            height: 200,
             child: CarouselSlider(
               items: imageSliders,
               carouselController: _controller,
               options: CarouselOptions(
-                  autoPlay: true,
-                  enlargeCenterPage: true,
-                  aspectRatio: 2.0,
-                  onPageChanged: (index, reason) {
-                    setState(() {
-                      _current = index;
-                    });
-                  }),
+                autoPlay: true,
+                enlargeCenterPage: true,
+                // You can also play with aspectRatio for better control
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _current = index;
+                  });
+                },
+              ),
             ),
           ),
           Row(
@@ -99,19 +93,21 @@ class _SliderviewState extends State<Sliderview> {
                 child: Container(
                   width: 12.0,
                   height: 12.0,
-                  margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                  margin: const EdgeInsets.symmetric(
+                      vertical: 8.0, horizontal: 4.0),
                   decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: (Theme.of(context).brightness == Brightness.dark
-                              ? Colors.white
-                              : Colors.black)
-                          .withOpacity(_current == entry.key ? 0.9 : 0.4)),
+                    shape: BoxShape.circle,
+                    color: (Theme.of(context).brightness == Brightness.dark
+                            ? Colors.white
+                            : Colors.black)
+                        .withOpacity(_current == entry.key ? 0.9 : 0.4),
+                  ),
                 ),
               );
             }).toList(),
           ),
-        ])
-      ],
+        ],
+      ),
     );
   }
 }
